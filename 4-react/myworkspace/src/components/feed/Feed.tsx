4b5075bd86f5a1cc4c9c25db1edb19bc40d9c98e
KO MYUNG HAN <komyunghan@gmail.com>
2021-08-27 02:57:05 +0900

    20210826

--- 4-react/myworkspace/src/components/feed/Feed.tsx
+++ 4-react/myworkspace/src/components/feed/Feed.tsx
@@ -1,17 +1,9 @@
 import { useRef, useState } from "react";
+import produce from "immer";
+import FeedEditModal from "./FeedEditModal";
+import { FeedState } from "./type/index";
 // import { lorem, penguin, robot } from "../common/data";
 // import { getTimeString } from "../common/lib/string";
-
-interface FeedState {
-  id: number;
-  content?: string | undefined;
-  dataUrl?: string | undefined;
-  fileType?: string | undefined;
-  createTime: number;
-  modifyTime?: number;
-  isEdit?: boolean;
-}
-
 const getTimeString = (unixtime: number) => {
   // Locale: timezone, currency 등
   // js에서는 브라우저의 정보를 이용함
@@ -21,7 +13,7 @@ const getTimeString = (unixtime: number) => {
 
 const Feed = () => {
   const [feedList, setFeedList] = useState<FeedState[]>([]);
-
+  const [isEdit, setIsEdit] = useState(false);
   const textRef = useRef<HTMLTextAreaElement>(null);
   const fileRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
@@ -59,14 +51,54 @@ const Feed = () => {
     // 입력값 초기화
     formRef.current?.reset();
   };
+  const del = (id: number, index: number) => {
+    console.log(id);
+    // immer로 state 배열 직접 조작(index로 삭제)
+    setFeedList(
+      produce((state) => {
+        state.splice(index, 1);
+      })
+    );
+  };
 
-  const del = (id: number) => {
-    setFeedList(feedList.filter((item) => item.id !== id));
+  // 컴포넌트가 업데이트 되도 유지시킬 수 있는 변수
+  // 수정할 todo객체
+  const editItem = useRef<FeedState>({ id: 0, dataUrl: "", createTime: 0 });
+  const edit = (item: FeedState) => {
+    // 수정할 todo객체
+    editItem.current = item;
+    // 모달 팝업을 보여주기
+    setIsEdit(true);
+  };
+  const save = (editItem: FeedState) => {
+    console.log(editItem);
+    setFeedList(
+      produce((state) => {
+        const item = state.find((item: { id: number; }) => item.id === editItem.id);
+        if (item) {
+          item.dataUrl = editItem.dataUrl;
+        }
+      })
+    );
+
+    // 모달창 닫기
+    setIsEdit(false);
   };
 
   return (
     <>
       <h2 className="text-center my-5">Feeds</h2>
+      {isEdit && (
+        <FeedEditModal
+          item={editItem.current}
+          onClose={() => {
+            setIsEdit(false);
+          }}
+          onSave={(editItem) => {
+            save(editItem);
+          }}
+        />
+      )}
       <form
         className="mt-5"
         onSubmit={(e) => {
@@ -99,7 +131,7 @@ const Feed = () => {
         </div>
       </form>
       <div className="mt-3">
-        {feedList.map((item) => (
+        {feedList.map((item, index) => (
           <div className="card mt-1" key={item.id}>
             {item.fileType &&
               (item.fileType?.includes("image") ? (
@@ -123,9 +155,26 @@ const Feed = () => {
                 </div>
                 <a
                   href="#!"
+                  className="btn btn-outline-secondary btn-sm text-nowrap me-1"
+                  onClick={(e) => {
+                    <input
+                      type="file"
+                      className="form-control me-1"
+                      accept="image/png, image/jpeg, video/mp4"
+                      ref={fileRef}
+                    />
+                    // 수정 모달 팝업 띄우고 데이터 객체 넘겨주기
+                    edit(item);
+
+                  }}
+                >
+                  수정
+                </a>
+                <a
+                  href="#!"
                   onClick={(e) => {
                     e.preventDefault(); // 기본 동작 방지
-                    del(item.id);
+                    del(item.id, index);
                   }}
                   className="link-secondary fs-6 text-nowrap"
                 >
